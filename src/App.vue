792e9db18804ace05ef996e237153cec2c27516b
PluginsKers <ikers@foxmail.com>
2022-06-17 14:37:22 +0800

    a LiveStream broadcasting room Created

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script lang="ts">
+import Video from './components/Video.vue';
+
+export default {
+	components: {
+		Video,
+	},
+
+	data() {
+		let loaded: boolean = false;
+		return { loaded };
+	},
+
+	mounted() {
+		setTimeout(() => {
+			this.loaded = true;
+		}, 1000);
+	},
+}
+
+</script>
+
+<template>
+	<div class="loading" v-if="!loaded">加载中...</div>
+	<Video v-else />
+</template>
+
+<style>
+@font-face {
+	font-family: "zj520";
+	src: url(./assets/fonts/font.ttf);
+}
+
+body,
+html {
+	margin: 0;
+	padding: 0;
+	background: rgb(41, 41, 41);
+}
+
+#app {
+	display: flex;
+	font-family: zj520, Avenir, Helvetica, Arial, sans-serif;
+	font-smooth: antialiased;
+	z-index: 1;
+	flex-direction: row;
+	align-items: center;
+	align-content: center;
+	justify-content: center;
+	flex-wrap: nowrap;
+}
+
+.loading {
+	position: absolute;
+	font-size: 30px;
+	top: 5vh;
+	left: 0;
+	width: 100vw;
+	text-align: center;
+	color: #fff;
+	z-index: 2;
+}
+</style>
---
+++ src/components/Video.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import HLSCore from '@cloudgeek/playcore-hls';
+const options = {
+	sources: [
+		{
+			src: "//stream.i5res.com:8080/hls/zj520.m3u8",
+			type: "application/x-mpegURL",
+		},
+	]
+}
+
+</script>
+
+<template>
+	<div class="player-wrap">
+		<vue3-video-player :core="HLSCore" :src="options.sources" title="直播间">
+		</vue3-video-player>
+	</div>
+</template>
+
+<style scoped>
+.player-wrap {
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background: #000;
+}
+</style>
